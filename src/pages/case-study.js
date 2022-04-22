68995587c52d903b9fa5428f5055c977632fa590
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-04-22 11:51:59 -0400

    Make site responsive up to tablet portrait (900px and up)

--- src/pages/case-study.js
+++ src/pages/case-study.js
@@ -3,30 +3,37 @@ import Layout from '../components/layout'
 import TableOfContents from '../components/tableOfContents'
 import { MDXProvider } from '@mdx-js/react'
 import CaseStudy from '../components/caseStudy.mdx'
+import './case-study.css'
 
 const CaseStudyPage = ({ location }) => {
   return (
     <Layout>
-      <TableOfContents location={location} />
-      <MDXProvider
-        components={{
-          h2: ({ children }) => {
-            return <h2 id={children.replaceAll(' ', '-').toLowerCase()}>{children}</h2>
-          },
-          h3: ({ children }) => {
-            return <h3 id={children.replaceAll(' ', '-').toLowerCase()}>{children}</h3>
-          },
-          img: ({ src, alt }) => {
-            return (
-              <div className="case-study__image-background">
-                <img src={src} alt="placeholder" />
-              </div>
-            )
-          },
-        }}
-      >
-        <CaseStudy />
-      </MDXProvider>
+      <div className="case-study__grid">
+        <div className="case-study__grid__left">
+          <TableOfContents location={location} />
+        </div>
+        <div className="case-study__grid__right">
+          <MDXProvider
+            components={{
+              h2: ({ children }) => {
+                return <h2 id={children.replaceAll(' ', '-').toLowerCase()}>{children}</h2>
+              },
+              h3: ({ children }) => {
+                return <h3 id={children.replaceAll(' ', '-').toLowerCase()}>{children}</h3>
+              },
+              img: ({ src, alt }) => {
+                return (
+                  <div className="case-study__image-background">
+                    <img src={src} alt="placeholder" />
+                  </div>
+                )
+              },
+            }}
+          >
+            <CaseStudy />
+          </MDXProvider>
+        </div>
+      </div>
     </Layout>
   )
 }
